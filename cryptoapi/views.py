46d52b4178b2e2104ead61dfd5eb6f0057c1ab09
Alankruthisaieni <alankruthisaieni@gmail.com>
2021-11-28 14:38:20 +0530

    Django App by fetching data from api

---
+++ cryptoapi/views.py
@@ -0,0 +1,12 @@
+import requests
+from django.conf import settings
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    # fetching the data
+    apidata = requests.get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false").json()
+    return render(request, 'index.html', {'apidata': apidata})
